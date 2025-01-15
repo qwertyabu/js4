19d349d3efdbd1b0d4b3e1f0b6ecafd0c4da6638
qwertyabu <monkey.famil4@gmail.com>
2025-01-15 21:42:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let admin = prompt("как тебя зовут?");
+
+let two = prompt("напиши любую букву");
+
+if (admin > two) {
+alert(`${admin} isminig ichida ${two} harifi mavjud`);    
+}
+else if(admin < two){
+alert(`${admin} isminig ichida ${two} harifi mavjud emas`);
+} else {
+alert(`${admin} isminig ichida ${two} harifi mavjud emas`)
+}
